448c372599dac65387eb5d120704a99e32869cbb
960bg <74264055+960bg@users.noreply.github.com>
2025-01-20 12:34:58 +0400

    refactor: 01-read-file: add comments

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 const filePath = path.resolve(__dirname, 'text.txt');
-// node 01-read-file
+
 // создать поток чтения из файла по пути filePath
 const readableStream = fs.createReadStream(filePath, 'utf8');
 
+// читать из потока
 async function readChunk(readStream) {
   for await (const element of readStream) {
     process.stdout.write(element);
